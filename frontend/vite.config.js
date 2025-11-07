d22c2aaf2b31f576ae54c6c171d83bdb518e750d
Ko <suchanya.pen@dome.tu.ac.th>
2025-11-07 12:00:06 +0700

    fix fontend vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
     port: 3000,
-    host: true
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://backend:5000",
+        changeOrigin: true,
+      },
+    },
   },
   build: {
-    outDir: 'dist'
-  }
-})
+    outDir: "dist",
+  },
+});
